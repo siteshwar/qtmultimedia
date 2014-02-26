adenexter/qtmultimedia-1

MER: Add enablers for working with camera rotation in gstreamer.
Includes a prototype control for reporting the orientation of the camera
sensor, and propagation of rotation information to the muxer element.

Change-Id: I7542cc54a253031b0692780f368cf31c65182d96

src/multimedia/controls/controls.pri
@@ -40,7 +40,8 @@ PUBLIC_HEADERS += \
 PRIVATE_HEADERS += \
     controls/qmediaplaylistcontrol_p.h \
     controls/qmediaplaylistsourcecontrol_p.h \
-    controls/qgstreamerelementcontrol_p.h
+    controls/qgstreamerelementcontrol_p.h \
+    controls/qcamerasensorcontrol_p.h
 
 SOURCES += \
     controls/qcameracapturebufferformatcontrol.cpp \
@@ -79,6 +80,7 @@ SOURCES += \
     controls/qaudioinputselectorcontrol.cpp \
     controls/qaudiooutputselectorcontrol.cpp \
     controls/qvideodeviceselectorcontrol.cpp \
-    controls/qgstreamerelementcontrol.cpp
+    controls/qgstreamerelementcontrol.cpp \
+    controls/qcamerasensorcontrol.cpp
 
 

siteshwar/qtmultimedia

src/multimedia/controls/controls.pri
@@ -39,7 +39,8 @@ PUBLIC_HEADERS += \
 
 PRIVATE_HEADERS += \
     controls/qmediaplaylistcontrol_p.h \
-    controls/qmediaplaylistsourcecontrol_p.h
+    controls/qmediaplaylistsourcecontrol_p.h \
+    controls/qcamerasensorcontrol_p.h
 
 SOURCES += \
     controls/qcameracapturebufferformatcontrol.cpp \
@@ -77,6 +78,7 @@ SOURCES += \
     controls/qaudioencodersettingscontrol.cpp \
     controls/qaudioinputselectorcontrol.cpp \
     controls/qaudiooutputselectorcontrol.cpp \
-    controls/qvideodeviceselectorcontrol.cpp
+    controls/qvideodeviceselectorcontrol.cpp \
+    controls/qcamerasensorcontrol.cpp
 
 